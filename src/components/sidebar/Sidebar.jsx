961ac6bfba224b8cc9d65b767dae47ded61f0ba2
didihottest <didihottest@gmail.com>
2022-06-02 21:33:24 +0700

    balikin jadi admin

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -34,7 +34,7 @@ function Sidebar() {
   return (
     <div className="sidebar">
       <div className="top">
-        <div className="logo">Dashboard</div>
+        <div className="logo">Admin</div>
       </div>
       <hr />
       <div className="center">
